0a5713bca994488ea26e64afd8f238ad056d92ff
zemaus <nhutkhoa3105@gmail.com>
2021-06-07 04:54:23 -0400

    copy actual class over

--- test/test_player.py
+++ test/test_player.py
@@ -1,68 +1,79 @@
-import pygame
-import os, sys
-
+import pygame, os, sys, math
 os.chdir('../game')
 
-class player(pygame.sprite.Sprite):
-    def __init__(self, health, stamina, bullets):
+class player_class(pygame.sprite.Sprite):
+    def __init__(self, health, attack_power):
         super().__init__()
-        self.player_state = [os.path.join('assets', 'player', 'normal_player.png'), 
+        self._player_state = [os.path.join('assets', 'player', 'normal_player.png'), 
                             os.path.join('assets', 'player', 'damaged_player.png'),
                             os.path.join('assets', 'player', 'attacking_player.png')]
-        self.taking_dmg_moveset = [] #to be fill in with int
-        self.attacking_moveset = [] #to be fill in with int
-        self.attack_state = False
-        self.damaged_state = False
+        
+        self._attack_power = attack_power
+        self._dir = 0 #0 for left, 1 for right
+        self._state = 0
+        self._frame = 0
         self.health = health
-        self.stamina = stamina
-        self.bullets = bullets
-        self.dir = 1 # 0 for left, 1 for right
-        self.change_state(0)
+        self.defending = False
+        self.defend_time = 0.1
 
-    #if attackin move player up a bit
-    #if takin dmg move player down a bit
-    #self.rect.move((x,y))
+        self.image = pygame.image.load(self._player_state[0]).convert()
+        self.image.set_colorkey((255, 255, 255), pygame.RLEACCEL)
+        self.rect = self.image.get_rect()
+
     def update(self):
-        if self.attack_state:
-            print("attackin")
-            self.attack_state = False
-        elif self.damaged_state:
-            print("damages")
-            self.damaged_state = False
-
-    #check mouse position, left click to interact.
-    def interact(self, target):
-        target.interact_w_player()
+        if self._state == 2: # 2 for attacking
+            move_dist = 10
+            if self._frame <= 15:
+                self.rect = self.rect.move((move_dist, -move_dist))
+            elif self._frame <= 30:
+                self.rect = self.rect.move((-move_dist, move_dist))
+            else:
+                self.change_state(0)
+                self._frame = 0
+            self._frame += 1
+        elif self._state == 1: # 1 for damaged
+            move_dist = 2
+            if self._frame <= 5:
+                self.rect = self.rect.move((-move_dist, move_dist))
+            elif self._frame <= 10:
+                self.rect = self.rect.move((move_dist, -move_dist))
+            else:
+                self.change_state(0)
+                self._frame = 0
+            self._frame += 1
 
     def take_damage(self, dmg):
         self.change_state(1)
-        self.damaged_state = True
-        self.health -= dmg
+        if self.defending:
+            dmg = math.floor(dmg/(2/self.defend_time))
+            self.health -= dmg
+        else:
+            self.health -= dmg
 
-    def attacking(self):
+
+    def attacking(self, target):
         self.change_state(2)
-        self.attack_state = True
+        target.take_damage(self._attack_power)
+
 
     def change_state(self, state_int):
-        self.image = pygame.image.load(self.player_state[state_int]).convert()
+        self.image = pygame.image.load(self._player_state[state_int]).convert()
         self.image.set_colorkey((255, 255, 255), pygame.RLEACCEL)
-        self.rect = self.image.get_rect()
+        self._state = state_int
 
     def movement(self):
         keys = pygame.key.get_pressed()
         speed = 10
         if keys[pygame.K_d]: # right key
-            if not self.dir: 
+            if not self._dir: 
                 self.image = pygame.transform.flip(self.image, 1, 0)
-                self.dir = 1
+                self._dir = 1
             self.rect = self.rect.move((speed, 0))
         elif keys[pygame.K_a]: # left key
-            if self.dir:
+            if self._dir:
                 self.image = pygame.transform.flip(self.image, 1, 0)
-                self.dir = 0
+                self._dir = 0
             self.rect = self.rect.move((-speed, 0))
-        elif keys[pygame.K_1]:
-            self.attacking()
 
     def update_location(self, x, y):
         self.rect.move_ip(x, y)
@@ -76,7 +87,7 @@ def main():
     screen.blit(background, (0, 0))
     pygame.display.flip()
 
-    user = player(10, 10, 10)
+    user = player_class(10, 10, 10)
     sprites = pygame.sprite.RenderPlain((user))
     user.update_location(540, 530)
     clock = pygame.time.Clock()
