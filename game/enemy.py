c4a47ecbd127716bd1866600fbeb9ae0e5306a9d
zemaus <nhutkhoa3105@gmail.com>
2021-06-07 02:40:58 -0400

    fix animation and add cooridate parameter

--- game/enemy.py
+++ game/enemy.py
@@ -1,31 +1,37 @@
 import pygame, os, sys, math
-class enemy(pygame.sprite.Sprite):
-    def __init__(self, health, attack_power):
+class enemy_class(pygame.sprite.Sprite):
+    def __init__(self, health, attack_power, combat_coord, sprite_location):
         super().__init__()
+        self.image = pygame.image.load(sprite_location).convert()
+        self.image.set_colorkey((255, 255, 255), pygame.RLEACCEL)
+        self.rect = self.image.get_rect()
         self._attack_power = attack_power
-        self._state = 0 #0 attackin, 0 taking dmg
+        self._state = 0 #2 attackin, 1 taking dmg
         self._frame = 0
+        self._defending = False
+        self._defend_time = 0.1
         self.health = health
-        self.defending = False
-        self.defend_time = 0.1
+        self.combat_coord = combat_coord
 
     def update(self):
-        if self._state == 2:
+        if self._state == 1: # 1 for damaged
+            move_dist = 2
             if self._frame <= 5:
-                self.rect.move(5, -5)
+                self.rect = self.rect.move((move_dist, -move_dist))
             elif self._frame <= 10:
-                self.rect.move(-5, 5)
+                self.rect = self.rect.move((-move_dist, move_dist))
             else:
                 self._state = 0
                 self._frame = 0
             self._frame += 1
-        elif self._state == 1:
-            if self._frame <= 5:
-                self.rect.move(-5, 5)
-            elif self._frame <= 10:
-                self.rect.move(5, -5)
+        elif self._state == 2: # 2 for attacking
+            move_dist = 10
+            if self._frame <= 15:
+                self.rect = self.rect.move((-move_dist, move_dist))
+            elif self._frame <= 30:
+                self.rect = self.rect.move((move_dist, -move_dist))
             else:
-                self._state = 0 
+                self._state = 0
                 self._frame = 0
             self._frame += 1
 
@@ -38,12 +44,12 @@ class enemy(pygame.sprite.Sprite):
             self.health -= dmg
 
     def attacking(self, target):
-        self._state = 0
+        self._state = 2
         target.take_damage(self._attack_power)
 
     def AI_logic(self):
         #add AI
         pass
 
-    def update_location(self, x, y):
-        self.rect.move_ip(x, y)
+    def update_location(self, coord):
+        self.rect.move_ip(coord)
