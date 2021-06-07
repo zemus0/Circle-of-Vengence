5801bd7e8e2183dd95fe41ec6a39917ae33725e6
zemaus <nhutkhoa3105@gmail.com>
2021-06-07 17:37:50 -0400

    change rat logic number

--- game/enemies.py
+++ game/enemies.py
@@ -6,4 +6,4 @@ class rat(enemy_class):
         super().__init__(50, 5, (1330, 150), os.path.join('assets', 'scene1', 'rat.png'))
     
     def AI_logic(self, enemy):
-        return super().AI_logic(enemy, 1, 0.1, 1.5, 1.8)
+        return super().AI_logic(enemy, 1, 0.2, 1.5, 1.8)
