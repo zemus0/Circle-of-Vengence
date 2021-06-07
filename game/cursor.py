b9324666622eb22a2ca5a453c3477cd9138e14c8
zemaus <nhutkhoa3105@gmail.com>
2021-06-07 03:42:38 -0400

    separate interact check and mouse movement

--- game/cursor.py
+++ game/cursor.py
@@ -6,12 +6,14 @@ class cursor(pygame.sprite.Sprite):
         self.image = pygame.image.load(os.path.join('assets', 'cursor_normal.png')).convert_alpha()
         self.rect = self.image.get_rect()
 
-    def update_cursor(self, interactables):
+    def interact_check(self, interactables):
         collide_check = self.rect.collidelist(interactables)
         if collide_check != -1:
             self.image = pygame.image.load(os.path.join('assets', 'cursor_selectable.png')).convert_alpha()
         else:
             self.image = pygame.image.load(os.path.join('assets', 'cursor_normal.png')).convert_alpha()
+        return collide_check
+    
+    def update(self):
         mouse_pos = pygame.mouse.get_pos()
         self.rect.midtop = mouse_pos
-        return collide_check
