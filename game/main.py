206c21e1b8d8fe86f624e87efb7df4910df15714
zemaus <nhutkhoa3105@gmail.com>
2021-06-07 02:48:07 -0400

    fix combat function

--- game/main.py
+++ game/main.py
@@ -1,16 +1,20 @@
 import pygame, os, sys, time, math
-from .player import player
-from .cursor import cursor
+from player import player_class
+from cursor import cursor
 
-#global variables
-interactable = []
-user = player(100, 20)
-mouse = cursor()
 
 def main():
+    global mainscreen
+    global mouse
+    global player
+    
     pygame.init()
     mainscreen = pygame.display.setmode((1600,900))
-    mainscreen.display.set_caption("Circle of Violence")
+    pygame.display.set_caption("Circle of Violence")
+    pygame.mouse.set_visible(False)
+
+    player = player_class(100, 5)
+    mouse = cursor()
 
 
 #def load_scene1():
@@ -18,14 +22,14 @@ def main():
 #    background = background.convert()
 
 
-def combat(screen, enemy):
-    sprites = pygame.sprite.RenderPlain((user, enemy))
-    user.update_location(560, 460)
-    enemy.update_location(1280, 90)
+def combat(enemy):
+    sprites = pygame.sprite.RenderPlain((player, enemy, mouse))
+    player.update_location(500, 400)
+    enemy.update_location(enemy.combat_location)
 
     clock = pygame.time.Clock()
     background = pygame.image.load(os.path.join('assets','battle scene.png')).convert()
-    screen.blit(background, (0, 0))
+    mainscreen.blit(background, (0, 0))
     pygame.display.flip()
 
     cooldown = False
@@ -41,26 +45,27 @@ def combat(screen, enemy):
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
 
-            if event.type == pygame.KEYDOWN and event.type == pygame.K_d: #defend 
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_d: #defend
                 timer = round(time.time() - time_defending_start, 2)
-                user.defend_time = timer if timer < 2 and timer > 0 else 2
-                user.defending = True
-            elif not (event.type == pygame.KEYDOWN and event.type == pygame.K_d):
+                player.defend_time = timer if timer < 2 and timer > 0 else 2
+                player.defending = True
+            elif not (event.type == pygame.KEYDOWN and event.key == pygame.K_d):
                 time_defending_start = time.time()
-                user.defend_time = 0
-                user.defending = False
-            if event.type == pygame.KEYDOWN and event.type == pygame.K_a: #attack
+                player.defend_time = 0
+                player.defending = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_a: #attack
                 if not cooldown:
-                    user.attacking(enemy)
-                    timer = math.floor(time.time())
+                    player.attacking(enemy)
+                    timer_attack = time.time()
                     cooldown = True
-                if cooldown and timer_attack - math.floor(time.time()) >= 5:
+                if cooldown and  time.time() - timer_attack >= 3:
                     cooldown = False
                     #add some sort of cool down graphics
 
+        sprites.update()
 
-        screen.blit(background, (0, 0))
-        sprites.draw(screen)
+        mainscreen.blit(background, (0, 0))
+        sprites.draw(mainscreen)
         pygame.display.update()
     
     pygame.quit()
