252de360b12d8c6e381352c0a94e4fffda01be98
zemaus <nhutkhoa3105@gmail.com>
2021-06-07 21:14:09 -0400

    move rat class

--- game/utils.py
+++ game/utils.py
@@ -1,4 +1,12 @@
-import pygame
+import pygame, os
+from enemy import enemy_class
+
+class rat_enemy(enemy_class):
+    def __init__(self):
+        super().__init__(os.path.join('assets', 'scene1', 'rat.png'), 50, 5, (1330, 150))
+    
+    def AI_logic(self, enemy):
+        return super().AI_logic(enemy, 1, 0.2, 1.5, 1.8)
 
 def draw_text(txt, font_size, padx, pady, width, screen, position, color=(0, 0, 0)):
     font = pygame.font.SysFont("monospace", font_size, True)
