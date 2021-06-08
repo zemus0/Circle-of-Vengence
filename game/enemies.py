b1339ebe08146feb27ed6bb980480dc1f43c8e55
zemaus <nhutkhoa3105@gmail.com>
2021-06-07 21:13:58 -0400

    remove unneeded

--- game/enemies.py
+++
@@ -1,9 +0,0 @@
-import os
-from enemy import enemy_class
-
-class rat_enemy(enemy_class):
-    def __init__(self):
-        super().__init__(os.path.join('assets', 'scene1', 'rat.png'), 50, 5, (1330, 150))
-    
-    def AI_logic(self, enemy):
-        return super().AI_logic(enemy, 1, 0.2, 1.5, 1.8)
