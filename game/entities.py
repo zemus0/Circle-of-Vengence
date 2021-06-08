6981199c468c92fe871a0057aa15d6b7637e6d24
zemaus <nhutkhoa3105@gmail.com>
2021-06-07 23:40:07 -0400

    add objects

--- game/entities.py
+++ game/entities.py
@@ -1,26 +1,47 @@
-import os
+import os, pygame
 from enemy import enemy_class
+from utils import draw_text
 
 class rat_enemy(enemy_class):
     def __init__(self):
         super().__init__(os.path.join('assets', 'scene1', 'rat.png'), 50, 5, (1330, 150))
-    
+        self._index = -1
+        self._dialogs = [
+            "You saw a rat chewing on something...",
+            "It hisses at you and charging in for the attack."
+        ]
+
+
     def AI_logic(self, enemy):
         return super().AI_logic(enemy, 1, 0.2, 1.5, 1.8)
 
+    def interact(self, text_box):
+        return super().interact(self._dialogs, text_box)
 
-class letter(enemy_class):
+
+class letter_class(enemy_class):
     def __init__(self, sprite_location):
         super().__init__(sprite_location)
-        self._index = 0
+        self._index = -1
         self._dialogs = [
             "You pick up a letter and it say:\nI know the location of your father's killer...",
             "Meet me at 17th street ChickTie Ave 7713.\nYour friendly comrade."
         ]
 
-    def interact(self, text_box, event):
-        interact_value = super().interact(self._dialogs, self._index , text_box, event)
-        if interact_value == True:
-            self._index += 1
-        elif interact_value == None:
-            text_box.set_alpha(0)
+    def interact(self, text_box):
+        return super().interact(self._dialogs, text_box)
+        
+class jacket_class(enemy_class):
+    def __init__(self, sprite_location):
+        super().__init__(sprite_location, alpha=True)
+        self._index = -1
+        self._dialogs = [
+            "You search through the pocket of your jacket and found...",
+            "50 bucks."
+        ]
+
+    def interact(self, text_box):
+        return super().interact(self._dialogs, text_box)
+
+        
+        
