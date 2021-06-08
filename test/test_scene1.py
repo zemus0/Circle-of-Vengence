4457f0509226b7c7deaa96b18603db306a42bf77
zemaus <nhutkhoa3105@gmail.com>
2021-06-08 16:24:11 -0400

    dont need test script anymore

--- test/test_scene1.py
+++ test/test_scene1.py
@@ -5,8 +5,7 @@ os.chdir('../game')
 from enemy import enemy_class
 from player import player_class
 from cursor import cursor
-from enemies import rat_enemy
-from utils import interactable
+from entities import *
 
 def main():
     global mainscreen
@@ -15,7 +14,7 @@ def main():
 
     pygame.init()
     mainscreen = pygame.display.set_mode((1600,900))
-    pygame.display.set_caption("test combat")
+    pygame.display.set_caption("test scene")
     pygame.mouse.set_visible(False)
     
     
@@ -23,10 +22,12 @@ def main():
     player = player_class(100, 20)
     scene1()
 
+
 def scene1():
     scene1_data = os.path.join('assets', 'scene1')
     background = pygame.image.load(os.path.join(scene1_data, 'background.png')).convert()
-    text_box = pygame.image.load(os.path.join('assets', 'dialog box.png')).convert_alpha()
+    text_box_original = pygame.image.load(os.path.join('assets', 'dialog box.png')).convert_alpha()
+    text_box = text_box_original.copy()
     text_box.set_alpha(0)
     mainscreen.blit(background, (0, 0))
     pygame.display.flip()
@@ -34,20 +35,25 @@ def scene1():
     player.update_location((540, 530))
     rat = rat_enemy()
     rat.update_location((65, 750))
-    jacket = enemy_class(os.path.join(scene1_data, 'jacket.png'), alpha=True)
+    jacket = jacket_class(os.path.join(scene1_data, 'jacket.png'))
     jacket.update_location((150, 270))
-    letter = enemy_class(os.path.join(scene1_data, 'letter.png'))
+    letter = letter_class(os.path.join(scene1_data, 'letter.png'))
     letter.update_location((1415, 717))
+    #door = door_class()
     interactables = [rat, jacket, letter]
     sprites = pygame.sprite.RenderPlain((rat, jacket, letter, player, mouse))
 
     clock = pygame.time.Clock()
     running = True
     quit_game = False
+    interact = False
+    finish_dialog = False
     while running:
         clock.tick(60)
-        for event in pygame.event.get():
+        if not interact:
             collision = mouse.interact_check(interactables)
+        
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 quit_game = True
@@ -55,23 +61,43 @@ def scene1():
                 running = False
                 quit_game = True
             
-            if event.type == pygame.MOUSEBUTTONDOWN and collision != -1:
-                interactables[collision].interact()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if collision != -1 and not interact:
+                    interact = True
+                if interact:
+                    text_box = text_box_original.copy()
+                    finish_dialog = interactables[collision].interact(text_box, event)
+                    if finish_dialog == True:
+                        text_box = text_box_original.copy()
+                        text_box.set_alpha(0)
+                        interact = False
+            
 
+
+        #rat, jacket, letter
+        if finish_dialog == True:
+            if collision == 0:
+                print("go to combat")
+                #combat(interactables[collision])
+                player.items['banana_bullet'] = True
+            elif collision == 1:
+                player.items['money'] = 50
+            elif collision == 2:
+                pass
+                #make the door interactable
         
         
         
         sprites.update()
         player.movement()
         
-
-        mainscreen.blits(((background, (0, 0)), (text_box, (0, 0))))
+        mainscreen.blits(((background, (0, 0)),(text_box, (0, 0))))
         sprites.draw(mainscreen)
         pygame.display.update()
     
     if quit_game:
         pygame.quit
-    
+
 
 if __name__ == "__main__":
     main()
